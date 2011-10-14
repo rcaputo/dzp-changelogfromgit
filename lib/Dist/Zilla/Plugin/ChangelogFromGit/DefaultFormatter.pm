9972a91b9e8347861649d983fe806727ca1b5535
Cory G Watson <gphat@Corys-iMac.local>
2011-10-14 10:12:06 -0500

    Un-hardcode the max_age and add the attribute to the role. Also add documentation.

--- lib/Dist/Zilla/Plugin/ChangelogFromGit/DefaultFormatter.pm
+++ lib/Dist/Zilla/Plugin/ChangelogFromGit/DefaultFormatter.pm
@@ -1,6 +1,8 @@
 package Dist::Zilla::Plugin::ChangelogFromGit::DefaultFormatter;
 use Moose;
 
+# ABSTRACT: Default formatter
+
 with 'Dist::Zilla::Plugin::ChangelogFromGit::Formatter';
 
 use Text::Wrap qw(wrap fill $columns $huge);
@@ -37,7 +39,7 @@ sub format {
 	    }
 	}
 	
-	my $max_age = 365;
+	my $max_age = $self->max_age;
     my $epilogue = "End of changes in the last " . $max_age . " day";
     $epilogue .= "s" unless $max_age == 1;
     
@@ -53,3 +55,42 @@ sub format {
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
+
+__END__
+
+=head1 NAME
+
+Dist::Zilla::Plugin::ChangelogFromGit::DefaultFormatter - Default formatting for the changelog
+
+=head1 SYNOPSIS
+
+In your dist.ini:
+
+	[ChangelogFromGit]
+	max_age     = 365
+	tag_regexp  = ^v\d+_\d+$
+	file_name   = CHANGES
+	wrap_column = 74
+
+The example values are the defaults.  This class is the default formatter,
+but may be explicitly set with:
+
+    formatter_class = Default
+
+=head1 DESCRIPTION
+
+The class implements the default format output by
+L<Dist::Zilla::PLugin::ChangelogFromGit>.
+
+=head1 AUTHOR
+
+Rocco Caputo <rcaputo@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Rocco Caputo.
+
+This is free software; you may redistribute it and/or modify it under
+the same terms as the Perl 5 programming language itself.
+
+=cut
